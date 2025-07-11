009b8df23d816e6f280725b9de433397408fed4a
Eli Prushansky <153029466+elirp1@users.noreply.github.com>
2025-07-11 16:59:20 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,7 +1,4 @@
 class WeatherService:
     def is_bad_weather(self, location, date_time):
-        # Placeholder for OpenWeatherMap API call
-        # For now, fake bad weather on Sundays
-        if date_time.weekday() == 6:
-            return True
-        return False
+        # Placeholder: Sundays = bad weather
+        return date_time.weekday() == 6
